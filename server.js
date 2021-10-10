999acd4cd2bdfd5a8533c85b9fda73d4c0dfb10c
Michael <mikerooze12@gmail.com>
2021-10-10 11:03:47 -0400

    Add recaptcha

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const emailer = require('./emailer');
+// recaptcha
+const request = require('request');
+
 
 const port = process.env.PORT || 3002;
 
@@ -22,7 +25,61 @@ app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+
+
 //handlers to respond to post request from client & send email
+
+// app.post('/', (req, res) => {
+//     console.log('Request received.');
+//     emailer.sendEmail(req, res);
+// });
+
+
+// recaptcha post
+
+app.post('/', (req, res) => {
+    console.log('Request received.');
+    if (
+        req.body.captcha === undefined ||
+        req.body.captcha === '' ||
+        req.body.captcha === null
+    ) {
+        return res.json({
+            "success": false,
+            "msg": "Please select captcha"
+        })
+    }
+
+    // Secret Key
+    const secretKey = '6LcmrrocAAAAALoZyr8K_H58pI0upUbl15enHYwB';
+
+    // Verify URL
+    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+    emailer.sendEmail(req, res);
+
+    // Make Request To VerifyURL
+    request(verifyUrl, (err, response, body) => {
+        body = JSON.parse(body)
+
+        // If Not Successful
+        if (body.success !== undefined && !body.success) {
+            return res.json({
+                "success": false,
+                "msg": "Failed Captcha Verification"
+            })
+        }
+
+        // If Successful
+
+        return res.json({
+            "success": true,
+            "msg": "Captcha Passed"
+        })
+
+    });
+
+});
+
 app.post('/', (req, res) => {
     console.log('Request received.');
     emailer.sendEmail(req, res);
